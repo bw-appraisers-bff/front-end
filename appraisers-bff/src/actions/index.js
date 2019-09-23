b28a8ed5f4910ad9638f5335d0035c5f195a373e
Brianna Keune <briannakeune@gmail.com>
2019-09-23 17:17:59 -0500

    imported actions to reducers

--- appraisers-bff/src/actions/index.js
+++ appraisers-bff/src/actions/index.js
@@ -1,18 +1,20 @@
-import axios from 'axios';
 import axiosWithAuth from '../utils/axiosWithAuth.js';
 
 export const LOGIN_START = 'LOGIN_START';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 
+//posting to BE api for token
 export const login = () => dispatch => {
     dispatch({ type: LOGIN_START });
-    axiosWithAuth
-        .post(`/auth/login`)
+    axios
+        // .post(`/auth/login`)
+        .post(`https://reqres.in/api/users/`)
         .then(res => console.log("LOGIN RES: ", res))
         .catch(err => dispatch({ type: LOGIN_FAIL, payload: err}))
 }
 
+//creating a user from BE api
 export const SIGNUP_START = 'SIGNUP_START';
 export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
 export const SIGNUP_FAIL = 'SIGNUP_FAIL';
@@ -25,6 +27,7 @@ export const signUp = () => dispatch => {
         .catch(err => dispatch({ type: SIGNUP_FAIL, payload: err }));
 }
 
+//sends information to BE -> DS to be put into algo
 export const POST_HOUSE_START = 'POST_HOUSE_START';
 export const POST_HOUSE_SUCCESS = 'POST_HOUSE_SUCCESS';
 export const POST_HOUSE_FAIL = 'POST_HOUSE_FAIL';
@@ -35,4 +38,30 @@ export const postHouse = () => dispatch => {
         .post(`/houses`)
         .then(res => console.log("POSTHOUSE RES: ", res))
         .catch(err => dispatch({ type: POST_HOUSE_FAIL, payload:err }));
+}
+
+// hoping this will get price from DS to display
+export const GET_PRICE_START = 'GET_PRICE_START';
+export const GET_PRICE_SUCCESS = 'GET_PRICE_SUCCESS';
+export const GET_PRICE_FAIL = 'GET_PRICE_FAIL';
+
+export const getPrice = () => dispatch => {
+    dispatch({ type: GET_PRICE_START });
+    axiosWithAuth
+        .get(`/prices`)
+        .then(res => console.log('GETPRICE: RES: ', res))
+        .catch(err => dispatch({ type: GET_PRICE_FAIL, payload: err }));
+}
+
+// saved properties
+export const GET_FAVORITES_START = 'GET_FAVORITES_START';
+export const GET_FAVORITES_SUCCESS = 'GET_FAVORITES_SUCCESS';
+export const GET_FAVORITES_FAIL = 'GET_FAVORITES_FAIL'; 
+
+export const getFavorites = () => dispatch => {
+    dispatch({ type: GET_FAVORITES_START });
+    axiosWithAuth
+        .get(`/users/:id/favories`)
+        .then(res => console.log('GETFAVORITE: RES: ', res))
+        .catch(err => dispatch({ type: GET_PRICE_FAIL, payload: err }));
 }
--- appraisers-bff/src/reducers/index.js
+++ appraisers-bff/src/reducers/index.js
@@ -1,5 +1,23 @@
 //would like to use a combineReducer but I'm going to start out with a large file will refactor
 
+import {
+    LOGIN_START,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    SIGNUP_START,
+    SIGNUP_SUCCESS,
+    SIGNUP_FAIL,
+    POST_HOUSE_START,
+    POST_HOUSE_SUCCESS,
+    POST_HOUSE_FAIL,
+    GET_PRICE_START,
+    GET_PRICE_SUCCESS,
+    GET_PRICE_FAIL,
+    GET_FAVORITES_START,
+    GET_FAVORITES_SUCCESS,
+    GET_FAVORITES_FAIL,
+} from '../actions';
+
 const initialState = {
     user: {
         id: '',
@@ -38,6 +56,7 @@ const initialState = {
 export const reducer = (state = initialState, action) => {
     switch (action.type) {
         
+
         default:
             return state;
     }

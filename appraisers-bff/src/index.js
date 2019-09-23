f41ff89989c1bf8f432f674f4aec23092caa2718
Brianna Keune <briannakeune@gmail.com>
2019-09-23 12:34:44 -0500

    added loginReducer start

--- appraisers-bff/src/index.js
+++ appraisers-bff/src/index.js
@@ -8,8 +8,14 @@ import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
+import reducer from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const state = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
 ReactDOM.render(
-  <Provider>
+  <Provider store={state}>
     <App />
   </Provider>,
   document.getElementById("root")
---
+++ appraisers-bff/src/reducers/loginReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    login: {
+        isLoggedIn: false,
+        isLogginIn: false,
+        error: ''
+    }
+}
+
+export const loginReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}

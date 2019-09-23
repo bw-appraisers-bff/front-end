5031160b630a05ec1c782d76d75ddaeeea289f3c
Vyraal1 <kevin.afable@gmail.com>
2019-09-23 16:49:49 -0400

    added markup for navbar

--- appraisers-bff/src/components/Navbar.js
+++ appraisers-bff/src/components/Navbar.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React from "react";
 
-const Navbar = () => {
-
-    return(
-        <div className="navbar red-outline">
-            <h3>Navigation Bar Component</h3>
+const Navbar = props => {
+  //setting props.isLoggedIn to true
+  //   props.isLoggedIn = true;
+  function NavLinks() {
+    if (!props.isLoggedIn) {
+      return (
+        <div className="navbar-links-signin">
+          <a>Log In</a>
+          <a>Sign Up</a>
         </div>
-    );
-}
+      );
+    } else {
+      return (
+        <div className="navbar-links-signin">
+          <a>Home</a>
+          <a>Appraise</a>
+          <a>Saved List</a>
+        </div>
+      );
+    }
+  }
+  return (
+    <div className="navbar">
+      <nav>
+        <div className="navbar-imgs">
+          <div className="icon-wrapper">
+            <img src="images/house-logo.png" alt="bff-icon"></img>
+          </div>
+          {/* <img src="images/bff-logo.png" alt="bff-appraiser-logo"></img> */}
+          <div className="logo-wrapper">
+            <p>BFF Appraisers </p>
+          </div>
+        </div>
+        <NavLinks />
+        {/* so do I put the conditonal rendering of links here? */}
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;

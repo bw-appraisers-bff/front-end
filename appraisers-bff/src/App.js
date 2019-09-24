0207d0589e1f078480fc45e4bc55dd1e505661be
Brianna Keune <briannakeune@gmail.com>
2019-09-23 19:00:46 -0500

    made App a class component

--- appraisers-bff/src/App.js
+++ appraisers-bff/src/App.js
@@ -9,19 +9,21 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Content from "./components/Content";
 
-function App() {
-  return (
-    <div className="app-container">
-      {/*NAVBAR COMPONENT: authenticated vs unauthenticated rendering */}
-      <Navbar />
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app-container">
+        {/*NAVBAR COMPONENT: authenticated vs unauthenticated rendering */}
+        <Navbar />
 
-      {/* Content COMPONENT: authenticated vs unauthenticated rendering */}
-      <Content />
+        {/* Content COMPONENT: authenticated vs unauthenticated rendering */}
+        <Content />
 
-      {/*FOOTER COMPONENT*/}
-      <Footer />
-    </div>
-  );
+        {/*FOOTER COMPONENT*/}
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;

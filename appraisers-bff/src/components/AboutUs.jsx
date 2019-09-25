f6609f4c9837191de8e83f64a1a5400f7cf019aa
MichaelHMods <mikeguadalupe@gmail.com>
2019-09-25 11:38:16 -0500

    About Us photos added. basic about page frame done.

---
+++ appraisers-bff/src/components/AboutUs.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+export default AboutUS => () {
+    return (
+        <div className="maincontainer">
+            <div className="aboutUs">
+                <div className="description-container">
+                    <h1>About the Team</h1>
+                    <p className="about-description">
+                    Bringing you a way to find the value you your home is a team of devs from across the nation. We strive to make sure that you get the best house estimate with the volumes of information in our grasp. Whether you are a buyer or seller we hop to bring you everything you need to get you the home that you want.
+                    </p>
+                </div>
+            </div>
+            <div className="photo-container">
+            
+            </div>
+
+
+    
+    </div>
+    )
+}

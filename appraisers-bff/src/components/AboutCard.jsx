f03b21be4295bff7165b3fc34a77f5d856c3341d
MichaelHMods <mikeguadalupe@gmail.com>
2019-09-26 06:51:52 -0500

    About page scaffolded, just needs styling

---
+++ appraisers-bff/src/components/AboutCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AboutCard = props => {
+    return(
+        <div className="about-card">
+            
+            {console.log("this in in return", props.title.github)}
+            <img src= {props.title.image} alt={props.title.name} className="bio-pic"/>
+            <h1 className="about-name">{props.title.name}</h1>
+            <p className="about-role">Role:{props.title.role}</p>
+            <a href={props.title.github}></a>
+        </div>
+
+
+    )
+}
+
+export default AboutCard;

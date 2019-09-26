868c6d5d47886c2c125e193362de40c92d23e621
Vyraal1 <kevin.afable@gmail.com>
2019-09-26 16:18:04 -0400

    updating destructured props

--- appraisers-bff/src/components/ResultCard.js
+++ appraisers-bff/src/components/ResultCard.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React from "react";
 
 const ResultCard = props => {
-  console.log("Result Cards: Props: ", props)
-  const {id, zipCode, yearBuilt, bedrooms, bathrooms, price, squareFootage} = props.house
+  console.log("Result Cards: Props: ", props);
+  const {
+    zipCode,
+    yearBuilt,
+    bedrooms,
+    bathrooms,
+    price,
+    squareFootage
+  } = props;
 
   // const dollarValue = price.toString();
 
   return (
     <div className="result-data">
-      {/* <h2>{`Appraised at $${dollarValue}`}</h2> */}
+      <h2>{`Appraised at $${price}`}</h2>
       <p>Your Appraisal Inputs:</p>
       <p>
-        {`${squareFootage} square foot home, built in ${yearBuilt} in zip code ${zipCode}` }
+        {`${squareFootage} square foot home, built in ${yearBuilt} in zip code ${zipCode}`}
       </p>
-      <p>
-        {`With ${bedrooms} bedrooms and ${bathrooms} bathrooms` }
-      </p>
+      <p>{`With ${bedrooms} bedrooms and ${bathrooms} bathrooms`}</p>
     </div>
   );
 };

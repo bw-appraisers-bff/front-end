7560e5c290f7a5fcf1f4f41c06d01c9cc6a38715
Vyraal1 <kevin.afable@gmail.com>
2019-09-26 11:27:35 -0400

    Temporarily removed savedcard pending animations

--- appraisers-bff/src/components/SavedCard.js
+++ appraisers-bff/src/components/SavedCard.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import { animated } from 'react-spring'
+import React from "react";
+import { animated } from "react-spring";
 
-import ResultCard from './ResultCard'
-import SaveEdit from './SaveEdit'
+import ResultCard from "./ResultCard";
+import SaveEdit from "./SaveEdit";
+import InterestForm from "./InterestForm";
 
 const SavedCard = props => {
   const { id, name, interestLevel, house } = props.result;
-  console.log("SavedCard in SavedList", props)
+  console.log("SavedCard in SavedList", props);
   return (
     <animated.div className="result-card" style={props.fadeIn}>
-      <div className="size-box">
+      {/* <div className="size-box">
         <SaveEdit name={name} interestLevel={interestLevel} />
-      </div>
+      </div> */}
+      <h2>{`Name: ${name} Interest: ${interestLevel}`}</h2>
+      <InterestForm />
       <ResultCard house={house} />
       <button>Delete</button>
     </animated.div>

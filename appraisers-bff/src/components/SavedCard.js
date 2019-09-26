1de5152b617d0c2b9eb6409ed80dbf3595cd200c
Brianna Keune <briannakeune@gmail.com>
2019-09-26 15:21:12 -0500

    will disregard these

--- appraisers-bff/src/components/SavedCard.js
+++ appraisers-bff/src/components/SavedCard.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { animated } from "react-spring";
 
 import ResultCard from "./ResultCard";
 import SaveEdit from "./SaveEdit";
 import InterestForm from "./InterestForm";
+import { connect } from "react-redux";
+import { putFav, deleteFav } from "../actions";
 
 //saved card still needs the history from result to display
 //we could save that to a local useState to be passed to a saved list
@@ -12,6 +14,9 @@ import InterestForm from "./InterestForm";
 const SavedCard = props => {
   const { id, name, interestLevel, house } = props.result;
   console.log("SavedCard in SavedList", props);
+
+  const [card, setCard] = useState(); //some prop I pull from reducer will go here
+
   return (
     <animated.div className="result-card" style={props.fadeIn}>
       {/* <div className="size-box">
@@ -25,4 +30,14 @@ const SavedCard = props => {
   );
 };
 
-export default SavedCard;
+const mapStateToProps = state => {
+  console.log("SavedCard: mstp: state: ", state);
+  return {
+    
+  }
+};
+
+export default connect(
+  mapStateToProps,
+  { putFav, deleteFav }
+)(SavedCard);
--- appraisers-bff/src/components/SavedList.js
+++ appraisers-bff/src/components/SavedList.js
@@ -8,60 +8,12 @@ import SavedCard from "./SavedCard";
 const SavedList = props => {
   console.log("SavedList: Props: ", props);
 
-  const { getFav, decodedToken } = props;
 
-  const dummyData = [
-    {
-      id: 1,
-      name: "First Saved",
-      interestLevel: 5,
-      house: {
-        id: 7,
-        zipCode: 90210,
-        yearBuilt: 1960,
-        squareFootage: 1000,
-        bedrooms: 10,
-        bathrooms: 5.5,
-        value: 100500
-      }
-    },
-    {
-      id: 2,
-      name: "Second Saved",
-      interestLevel: 3,
-      house: {
-        id: 8,
-        zipCode: 10024,
-        yearBuilt: 1975,
-        squareFootage: 2500,
-        bedrooms: 1,
-        bathrooms: 0.5,
-        value: 100500
-      }
-    },
-    {
-      id: 3,
-      name: "Third Saved",
-      interestLevel: 1,
-      house: {
-        id: 9,
-        zipCode: 60007,
-        yearBuilt: 1920,
-        squareFootage: 5200,
-        bedrooms: 3,
-        bathrooms: 2,
-        value: 100500
-      }
-    }
-  ];
+  const { getFav, decodedToken, favorites } = props;
 
-  const fadeIn = useSpring({
-    opacity: 1,
-    from: { opacity: 0 },
-    config: { mass: 1, tension: 140, friction: 70 }
-  });
+  console.log("Favourites!!!!, ", favorites)
 
-  const [savedResults, setSavedResults] = useState(dummyData);
+  const [savedResults, setSavedResults] = useState(favorites);
   //learning how to avoid merge conflicts
 
   const tokenObj = {
@@ -75,11 +27,8 @@ const SavedList = props => {
 
   return (
     <div className="saved">
-      {savedResults.map(result => (
-        // <Link to={`movies/${movie.id}`}>
-        <SavedCard {...props} key={result.id} result={result} fadeIn={fadeIn} />
-        // </Link>
-      ))}
+      {favorites.map(result => {
+        console.log("INSIDE MAP: result, ", result); })}
     </div>
   );
 };

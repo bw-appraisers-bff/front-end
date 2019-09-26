08c0a5f96a530c2fa357437b78d8d2013a1c0dde
gelinas <jlgelinas@gmail.com>
2019-09-25 21:52:47 -0400

    Got edit-on-click working for savedResults

---
+++ appraisers-bff/src/components/SaveEdit.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import {useSpring, useTransition, animated } from 'react-spring'
+
+const SaveEdit = (props) => {
+
+    const name = props.name;
+    const interestLevel = props.interestLevel;
+    const [toggle, set] = useState(true);
+    const onClick = () => set(!toggle);
+    const transitions = useTransition(toggle, null, {
+    from: { position: 'absolute', opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    config: { mass: 1, tension: 280, friction: 70 }
+    });
+    
+
+    return transitions.map(({ item, key, props }) => 
+    item
+    ? <animated.div style={props} onClick={onClick} className="size-box">
+        <h2>{`Name: ${name} Interest: ${interestLevel}`}</h2>
+        </animated.div>
+    : <animated.div style={props} onClick={onClick} className="size-box">
+        <span>Form will go here</span><button>Submit</button>
+        </animated.div>
+    );
+}
+
+export default SaveEdit;
+
--- appraisers-bff/src/components/SavedCard.js
+++ appraisers-bff/src/components/SavedCard.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { animated } from 'react-spring'
 
 import ResultCard from './ResultCard'
+import SaveEdit from './SaveEdit'
 
 const SavedCard = props => {
   const { id, name, interestLevel, house } = props.result;
   console.log("SavedCard in SavedList", props)
   return (
     <animated.div className="result-card" style={props.fadeIn}>
-      <div className="data-wrapper">
-        <h2>{`Name: ${name}`}</h2>
-        <h2>{`Interest Level: ${interestLevel}`}</h2>
+      <div className="size-box">
+        <SaveEdit name={name} interestLevel={interestLevel} />
       </div>
       <ResultCard house={house} />
     </animated.div>

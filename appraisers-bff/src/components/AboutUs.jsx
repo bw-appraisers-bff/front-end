150046e1ef19c9da6a83295b598ad37f3b40d594
MichaelHMods <mikeguadalupe@gmail.com>
2019-09-26 10:07:03 -0500

    draft of the about page.

--- appraisers-bff/src/components/AboutUs.jsx
+++ appraisers-bff/src/components/AboutUs.jsx
@@ -4,7 +4,7 @@ import AboutCard from "./AboutCard";
 
 const AboutUs = ( ) => {
     const teamData = [
-        {image: "../../../images/teamPics/MichaelFinal.png", name: "Michael Guadalupe", role: "Front End", github: "https://github.com/MichaelHMods" },
+        
         {image: "../../../images/teamPics/KevinFinal.png", name: "Kevin Afable", role: "Frontend", github: "https://github.com/Vyraal1" },
         {image: "../../../images/teamPics/LouisFinal.png", name: "Louis Galinas", role: "Frontend", github: "https://github.com/gelinas" },
         {image: "../../../images/teamPics/BriannaFinal.png", name: "Brianna Keune", role: "Frontend", github: "https://github.com/briannakeune"},
@@ -14,7 +14,8 @@ const AboutUs = ( ) => {
         {image: "../../../images/teamPics/MatthewFinal.png", name: "Matthew Mauney", role: 'Data Engineer', github: "https://github.com/mauney "},
         {image: "../../../images/teamPics/TaylorFinal.png", name: 'Taylor Bickell', role: 'Machine Learning', github: "https://github.com/tcbic" },
         {image: "../../../images/teamPics/MarvinFinal.png", name: 'Marvin Davila', role: 'Machine Learning', github: 'https://github.com/MAL3X-01' }, 
-        
+        {image: "../../../images/teamPics/MichaelFinal.png", name: "Michael Guadalupe", role: "Front End", github: "https://github.com/MichaelHMods" }
+
     ]
 
    console.log(teamData)

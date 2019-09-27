7f79d577f39ccaf72db43cc1257b1c740bbfa8d3
Brianna Keune <briannakeune@gmail.com>
2019-09-27 01:05:28 -0500

    dark-mode is functional, just not pretty

--- appraisers-bff/src/hooks/useDarkMode.js
+++ appraisers-bff/src/hooks/useDarkMode.js
@@ -3,10 +3,11 @@ import { useLocalStorage } from "./useLocalStorage.js";
 
 export const useDarkMode = initialValues => {
   const [darkMode, setDarkMode] = useLocalStorage("dark-mode", initialValues);
-
+  const formContainer = document.getElementsByClassName("form-container")
   useEffect(() => {
     if (darkMode === true) {
       document.body.classList.add("dark-mode");
+      // formContainer.classList.add("dark-mode");
     } else {
       document.body.classList.remove("dark-mode");
     }

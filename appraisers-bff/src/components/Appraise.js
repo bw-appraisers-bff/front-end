35e4d910abc4a1b1bf2ded96637364d2f7748165
gelinas <jlgelinas@gmail.com>
2019-10-02 10:40:50 -0400

    reduced wakeup call to just backend

--- appraisers-bff/src/components/Appraise.js
+++ appraisers-bff/src/components/Appraise.js
@@ -10,16 +10,6 @@ const Appraise = props => {
         config: { mass: 1, tension: 140, friction: 70 }
       });
 
-    // useEffect(() => {
-    //     //wake up datscience model 
-    //     axios
-    //         .get(`https://appraisers-bff.herokuapp.com/?&yearbuilt=1983&bedrooms=1&bathrooms=2&squarefeet=1800`)
-    //         .then(res => {
-    //         console.log("datascience model awake", res)
-    //         })
-    //         .catch(err => console.log(err));
-    //     }, []);
-
     return (
         <animated.div className="form-container" style={fadeIn}>
             <h2>Appraise Now</h2>

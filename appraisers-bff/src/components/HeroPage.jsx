7107f117c1c5d9e085f4db08de2c4e01b5acf8ce
Brianna <briannakeune@gmail.com>
2019-11-28 14:19:36 -0600

    finished image path

--- appraisers-bff/src/components/HeroPage.jsx
+++ appraisers-bff/src/components/HeroPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function HeroPage() {
     return (
         <div>
-            <img src="images/appraiser-bff-home-page-image" alt="View of homepage for link previews"/>
+            <img src="images/appraiser-bff-home-page.png" alt="View of homepage for link previews"/>
         </div>
     )
 }
